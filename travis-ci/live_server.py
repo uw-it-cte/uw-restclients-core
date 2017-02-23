62b9abb7ec3a1b3a9502e37b311121355babe750
Patrick Michaud <pmichaud@uw.edu>
2017-02-23 15:21:13 -0800

    py2/3 for the fake live server

--- travis-ci/live_server.py
+++ travis-ci/live_server.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
-from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+try:
+    from http.server import BaseHTTPRequestHandler, HTTPServer
+except ImportError:
+    from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
 
 PORT_NUMBER = 9876
 
@@ -10,18 +13,18 @@ class myHandler(BaseHTTPRequestHandler):
             self.send_header('Content-type','text/html')
             self.send_header('X-Custom-Header','header-test')
             self.end_headers()
-            self.wfile.write("ok")
+            self.wfile.write(b"ok")
             return
         elif self.path == "/403":
             self.send_response(403)
             self.end_headers()
-            self.wfile.write("Forbidden")
+            self.wfile.write(b"Forbidden")
             return
 
         self.send_response(404)
         self.send_header('Content-type','text/html')
         self.end_headers()
-        self.wfile.write("Not Found")
+        self.wfile.write(b"Not Found")
         return
 
 
