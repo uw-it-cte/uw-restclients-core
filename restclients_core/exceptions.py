7c2faf60e6b2aeb65db4208cdf333c91eae1eb84
Patrick Michaud <pmichaud@uw.edu>
2017-02-28 09:49:00 -0800

    Moving over the data failure exception

--- restclients_core/exceptions.py
+++ restclients_core/exceptions.py
@@ -1,3 +1,20 @@
 class ImproperlyConfigured(Exception):
     """Required setting is missing"""
     pass
+
+
+class DataFailureException(Exception):
+    """
+    This exception means there was an error fetching content
+    in one of the rest clients.  You can get the url that failed
+    with .url, the status of the error with .status, and any
+    message with .msg
+    """
+    def __init__(self, url, status, msg):
+        self.url = url
+        self.status = status
+        self.msg = msg
+
+    def __str__(self):
+        return ("Error fetching %s.  Status code: %s.  Message: %s." %
+                (self.url, self.status, self.msg))
