3a49afb2c7a4a6df9b8a309424feb300d7c204ed
Ethan Turner <ezturner@washington.edu>
2017-06-02 15:08:48 -0700

    Removed overriden urlencode

--- restclients_core/util/__init__.py
+++ restclients_core/util/__init__.py
@@ -1,10 +1,7 @@
-import sys
 try:
-    from urllib import quote_plus
-    from urllib import urlencode as urllib_encode
+    from urllib import urlencode
 except ImportError:
-    from urllib.parse import urlencode as urllib_encode
-    from urllib.parse import quote_plus
+    from urllib.parse import urlencode
 
 
 def url_with_query(base, params):
@@ -13,22 +10,3 @@ def url_with_query(base, params):
     formatted url. Will be consistent between python 2 and 3.
     """
     return base + "?%s" % urlencode(params)
-
-
-def urlencode(query, doseq=True, safe='', encoding=None, errors=None,
-              quote_via=quote_plus):
-    """
-    This is a utility method used to ensure that the query params are sorted
-    consistently between python2 and python3 by urlencode.
-    """
-    if type(query) is dict:
-        query = [(key, query[key]) for key in sorted(query.keys())]
-    elif type(query) is list:
-        query = sorted(query, key=lambda tup: tup[0])
-
-    if (sys.version_info > (3, 5)):
-        return urllib_encode(query, doseq, safe, encoding, errors, quote_via)
-    elif (sys.version_info == (3, 4)):
-        return urllib_encode(query, doseq, safe, encoding, errors)
-    else:
-        return urllib_encode(query, doseq)
