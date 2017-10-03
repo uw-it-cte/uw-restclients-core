4895dc800272439f41f4274f5a11de85b28fc615
Ethan Turner <ezturner@washington.edu>
2017-10-03 15:52:18 -0700

    Fixing edge case related to file resolution

--- restclients_core/util/mock.py
+++ restclients_core/util/mock.py
@@ -31,11 +31,11 @@ def load_resource_from_path(resource_dir, service_name,
 
         # attempt to open query permutations even on success
         # so that if there are multiple files we throw an exception
-        if "?" in url:
+        if handle is None and "?" in url:
             handle = attempt_open_query_permutations(url, orig_file_path,
                                                      False)
 
-        if "?" in url:
+        if header_handle is None and "?" in url:
             header_handle = attempt_open_query_permutations(url,
                                                             orig_file_path,
                                                             True)
