64b3ed77b340c340fa1975f43e607000b2e06a66
Ethan Turner <ezturner@washington.edu>
2017-10-11 14:38:48 -0700

    Adjusting test

--- restclients_core/tests/test_timing_log.py
+++ restclients_core/tests/test_timing_log.py
@@ -14,9 +14,10 @@ class TestTimingLog(TestCase):
                        RESTCLIENTS_TIMING_LOG_ENABLED=True,
                        RESTCLIENTS_TIMING_LOG_RATE=1.0)
     def test_dont_log_date_range(self):
-        with self.assertLogs('restclients_core.dao', level='INFO') as cm:
-            response = TDAO().getURL('/ok')
-            self.assertEquals(len(cm.output), 0)
+        with self.assertRaises(AssertionError):
+            with self.assertLogs('restclients_core.dao', level='INFO') as cm:
+                response = TDAO().getURL('/ok')
+                self.assertEquals(len(cm.output), 0)
 
     @skipUnless(hasattr(TestCase, 'assertLogs'), 'Python < 3.4')
     @override_settings(RESTCLIENTS_TIMING_START=(datetime.datetime.now() - datetime.timedelta(1)).isoformat(),
